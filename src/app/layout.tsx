c709a82448881b6b1dd2531496dd40e69f2e5522
caishen <caishen@greatonce.com>
2024-10-01 13:57:08 +0800

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import {Inter} from 'next/font/google'
 const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-    title: '宅生活超市 - 即时零售，便捷生活',
-    description: '加入宅生活超市，开启您的即时零售创业之旅。低投资高回报，完善的培训支持，先进的技术平台。',
+    title: '宅当家超市 - 即时零售，便捷生活',
+    description: '加入宅当家超市，开启您的即时零售创业之旅。低投资高回报，完善的培训支持，先进的技术平台。',
 }
 
 export default function RootLayout({
